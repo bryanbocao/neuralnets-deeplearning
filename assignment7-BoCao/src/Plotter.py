bb89e5dd66f23a8b82725e53321b08a74e04bcf7
BryanBo-Cao <bryan.bo.cao@outlook.com>
2017-12-15 21:35:14 -0700

    Assign7 save results

--- assignment7-BoCao/src/Plotter.py
+++ assignment7-BoCao/src/Plotter.py
@@ -9,18 +9,21 @@ Reference:
 import matplotlib.pyplot as plt
 
 def plot_2lists(x, y, x_label, y_label):
+    plt.figure(figsize=(10, 6))
     plt.xlabel(x_label)
     plt.ylabel(y_label)
     plt.plot(x, y, 'ro')
     plt.show()
     
 def plot_4lists(alpha_x, alpha_y, beta_x, beta_y, x_label, y_label):
+    plt.figure(figsize=(10, 6))
     plt.xlabel(x_label)
     plt.ylabel(y_label)
     plt.plot(alpha_x, alpha_y, 'ro', beta_x, beta_y, 'bo')
     plt.show()
 
 def plot_acc_distance(data):
+    plt.figure(figsize=(10, 6))
     plt.xlabel("Distance")
     plt.ylabel("Human Judgement Accuracy")
     
@@ -45,6 +48,7 @@ def plot_acc_distance(data):
     plt.show()
     
 def plot_acc_cosine(data):
+    plt.figure(figsize=(10, 6))
     plt.xlabel("Cosine")
     plt.ylabel("Human Judgement Accuracy")
     
@@ -69,6 +73,7 @@ def plot_acc_cosine(data):
     plt.show()
     
 def plot_prob_distance(data):
+    plt.figure(figsize=(10, 6))
     plt.xlabel("Distance")
     plt.ylabel("Probability of human judgement on similarity")
     
@@ -93,6 +98,7 @@ def plot_prob_distance(data):
     plt.show()
     
 def plot_prob_cosine(data):
+    plt.figure(figsize=(10, 6))
     plt.xlabel("Cosine")
     plt.ylabel("Probability of human judgement on similarity")
     
